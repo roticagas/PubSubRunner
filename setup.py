789d986912de1a58b4875ae10362d39804e091c2
Roticagas <roticagas@gmail.com>
2019-01-04 11:32:24 +0700

    add files for python package

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PubSubRunner",
+    version="1.0.0",
+    author="Chettha Thawornsathit",
+    author_email="roticagas@gmail.com",
+    description="Python Boilerplate for Google Cloud PubSub running task in python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roticagas/PubSubRunner",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
