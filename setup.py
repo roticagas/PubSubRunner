efecd0b82c9e9ff6a3511a9b7bfb965e72f49f90
Roticagas <roticagas@gmail.com>
2019-01-16 14:14:40 +0700

    1.1.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PubSubRunner",
-    version="1.1.7",
+    version="1.1.8",
     author="Chettha Thawornsathit",
     author_email="roticagas@gmail.com",
     description="Python Boilerplate for Google Cloud PubSub running task in python",
